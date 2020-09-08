fe6abf7ec31c39c84efb7304bd25d7254e005d8c
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-09-08 15:46:02 -0400

    added scene switches

---
+++ MazeGame/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public string sceneName; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckStart();
+    }
+
+    void CheckStart()
+    {
+        if (Input.GetMouseButtonDown (0)) 
+        {
+            //Change the scene name depending on which script you are on.
+            //Note that this will require that each SceneManager has a string
+            //typed into the Unity Inspector for sceneName.
+            SceneManager.LoadScene (sceneName);
+        }
+    }
+}
