15d0f28b8543f83de476a15b53254c66181e984c
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-09-08 16:38:55 -0400

    added UI

--- MazeGame/Assets/Scripts/RandomCollectible.cs
+++ MazeGame/Assets/Scripts/RandomCollectible.cs
@@ -4,26 +4,16 @@ using UnityEngine;
 
 public class RandomCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        RepositionCollectible();
+    }
     void RepositionCollectible()
     {
         float newX = Random.Range (-5, 5);
         float newY = Random.Range (-3, 3);
         Vector2 newPos = new Vector2 (newX, newY);
         transform.position = newPos;
-    }
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        RepositionCollectible();
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject.Find ("Score_Canvas").GetComponent<ScoreScript> ().AddScore ();
     }
 }
---
+++ MazeGame/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text scoreText;
+    int score = 0;
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = score.ToString ();
+    }
+}
