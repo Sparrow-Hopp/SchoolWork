09f82bde7b13f32c5014097ef5f5bb9f71698360
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-09-08 14:54:37 -0400

    adding object movement with phsyics

---
+++ MazeGame/Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    float xMove = 0f;
+    float yMove = 0f;
+    Rigidbody2D rb;
+    public float speed = 10f;
+
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        MovePlayer();
+    }
+    void CheckInput()
+    {
+       xMove = Input.GetAxis ("Horizontal");
+       yMove = Input.GetAxis ("Vertical");
+    }
+    void MovePlayer()
+    {
+       rb.AddForce (new Vector2 (xMove, yMove) * speed);
+    }
+}
