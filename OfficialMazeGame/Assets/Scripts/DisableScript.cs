ed1894fe8d1fb4af67b83ecefdb3ff51b8022d7b
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-09-12 15:56:01 -0400

    added triggers and disabling walls

---
+++ OfficialMazeGame/Assets/Scripts/DisableScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableScript : MonoBehaviour
+{
+    public GameObject gameObj1;
+    public GameObject gameObj2;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        gameObj1.SetActive(false);
+        gameObj2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
