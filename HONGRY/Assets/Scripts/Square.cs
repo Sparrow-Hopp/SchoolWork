3b67302d4bba6293d03ffcbc461c6c2d3ebaac1f
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-10-31 23:24:22 -0400

    added mouse over

--- HONGRY/Assets/Scripts/Square.cs
+++ HONGRY/Assets/Scripts/Square.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class Square : MonoBehaviour
 {
     public Vector2Int gridPosition;
+    public Material mouseOverMaterial; //Drag MouseOverMaterial into Square prefab's Square script to assign.
+
+    //When the mouse hovers over the GameObject, it turns to this color
+    static Color mouseOverColor;
+
+    //This stores the GameObject’s original color
+    Color originalColor;
+
+    //Get the GameObject’s spriteRenderer to access the GameObject’s material and color
+    SpriteRenderer spriteRenderer; //A spriteRenderer to get and swap colors
 
     //OnMouseOver() tells Unity what should happen when a mouse is over the object
     void OnMouseOver()
     {
         Debug.Log(name); //unity implicitly defines "name" for you to get the "name" of the object.
         GridManager.UpdateUI(this); //the keyword "this" refers to the current object, aka "this" object.
+        spriteRenderer.material.color = mouseOverColor; //change color while mouse is over the object
     }
 
     void OnMouseExit()
     {
         GridManager.UpdateUI();
+        spriteRenderer.material.color = originalColor;
+    }
+
+    void Awake()
+    {
+        //Get the spriteRenderer component from the GameObject
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //Get the original color of the GameObject
+        originalColor = spriteRenderer.material.color;
+        mouseOverColor = mouseOverMaterial.color;
+
+        //mouseOverColor = Resources.Load<Material>("Materials/MouseOverColor").color;
+        // avoid using Resources if you want to build the project see:
+        // https://unity3d.com/learn/tutorials/topics/best-practices/resources-folder
     }
 }
