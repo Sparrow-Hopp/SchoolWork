a8d5ddf7bccff448beb95506c9dbe614548e2682
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-11-30 20:09:13 -0500

    added dummy sprites and player movement script

---
+++ FinalGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float xMove = 0f, yMove = 0f;
+    public float xSpeed = 100f, ySpeed = 100f;
+    Rigidbody2D myRigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void CheckInput()
+    {
+        xMove = Input.GetAxis("Horizontal") * xSpeed;
+        yMove = Input.GetAxis("Vertical") * ySpeed;
+    }
+
+    void Move()
+    {
+        myRigidbody2D.velocity = new Vector2(xMove, yMove);
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+}
