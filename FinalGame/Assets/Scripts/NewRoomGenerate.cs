811d5b2f62171b80c5377fc851b05e3297b14813
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-12-01 19:16:46 -0500

    made infinitely generating dungeon, past rooms get deleted

---
+++ FinalGame/Assets/Scripts/NewRoomGenerate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRoomGenerate : MonoBehaviour
+{
+    public GameObject roomNew, roomOld;
+    public float scalerX, scalerY;
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Instantiate(roomNew, new Vector3 (transform.position.x + scalerX, transform.position.y + scalerY, 0), transform.rotation);
+            Destroy(roomOld);
+        }
+    }
+}
