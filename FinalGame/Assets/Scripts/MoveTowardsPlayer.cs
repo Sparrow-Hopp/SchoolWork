d09a2f9cf0280db69b0001f48bb90bcb9e3a9705
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-12-12 13:44:49 -0500

    made enemies spawn in world space, made them bullet sponges

---
+++ FinalGame/Assets/Scripts/MoveTowardsPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardsPlayer : MonoBehaviour
+{
+    GameObject target;
+    public float speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LerpPlayer();
+    }
+
+    public void LerpPlayer()
+    {            
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Time.deltaTime * speed);
+    }
+}
---
+++ FinalGame/Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemy;
+    public float lowXBound, lowYBound, hiXBound, hiYBound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(enemy, new Vector3(Random.Range(lowXBound, hiXBound), Random.Range(lowYBound, hiYBound), 0), transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
