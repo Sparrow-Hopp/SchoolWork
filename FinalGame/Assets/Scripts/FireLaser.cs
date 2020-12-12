ca79455fd8868c2bf665b6662c313090c8ec753c
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-12-12 12:28:55 -0500

    added particle effects onto spells, working on movement

---
+++ FinalGame/Assets/Scripts/FireLaser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireLaser : MonoBehaviour
+{
+    public GameObject laserPrefab;
+    Transform laserSpawn;
+
+    void Start()
+    {
+        laserSpawn = transform.Find("LaserSpawn");
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab, laserSpawn.position, laserSpawn.rotation) as GameObject;
+    }
+}
---
+++ FinalGame/Assets/Scripts/MoveLaser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLaser : MonoBehaviour
+{
+    //public float offset;
+    public float thrust;
+    Vector3 mouseScreenPosition;
+    Vector2 mouse;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find ("Player");
+        mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+     	mouse = new Vector2 (mouseScreenPosition.x, mouseScreenPosition.y);
+        //float force = Mathf.Atan2(lookAt.y - this.transform.position.y, lookAt.x - this.transform.position.x) + offset;
+        //float forceDir = (180 / Mathf.PI) * force;
+        GetComponent<Rigidbody2D>().AddForce((mouseScreenPosition - player.transform.position) * thrust);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+}
