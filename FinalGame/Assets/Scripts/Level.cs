d3685a1a787da252d14ab7e85579e34164b5eaa8
Tempest-Hopp <leah.hopp@mymail.champlain.edu>
2020-12-14 16:33:29 -0500

    Made game over scenes functional

---
+++ FinalGame/Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    public Text level;
+
+    void Start()
+    {
+        level.text = GameManager.playerLevel.ToString();
+    }
+}
